2db60680cdbdc2ab51e4154b0ddf1880ee2a4959
Owen Shepherd <414owen@gmail.com>
2016-10-03 17:06:29 +0100

    Added custom messages. Patrick will appreciate a personal touch.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SpamPatrick
 
-SpamPatrick is a script that uses the email form on Patrick O'Boyle's website ([http://patrickoboyle.com/](http://patrickoboyle.com/)) to send him an email every *random number between 1 and 11* seconds.
+SpamPatrick is a script that uses the email form on Patrick O'Boyle's website ([http://patrickoboyle.com/](http://patrickoboyle.com/)) to send him an email every five seconds.
 
 It picks a random name, company and email suffix from some long lists.
 
--- spampatrick.sh
+++ spampatrick.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+# Taken from StackOverflow
+urlencode() {
+    # urlencode <string>
+    old_lc_collate=$LC_COLLATE
+    LC_COLLATE=C
+    
+    local length="${#1}"
+    for (( i = 0; i < length; i++ )); do
+        local c="${1:i:1}"
+        case $c in
+            [a-zA-Z0-9.~_-]) printf "$c" ;;
+            *) printf '%%%02X' "'$c" ;;
+        esac
+    done
+    
+    LC_COLLATE=$old_lc_collate
+}
+
+echo "Enter a message for Patrick"
+message=`urlencode "$(cat /dev/stdin)"`
+echo "$message"
+
 printf "Initialising spam engine v1.0\n\n"
 names=`cat names.txt`
 companies=`cat companies.txt`
@@ -14,7 +36,7 @@ while true; do
 	suffix=`echo "$suffixes" | shuf -n 1`
 	reason=`echo "$reasons" | shuf -n 1`
 
-	curl 'http://patrickoboyle.com/send_contact_form_email.php' -H 'Host: patrickoboyle.com' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: http://patrickoboyle.com/' -H 'Cookie: _ga=GA1.2.1618244444.1475148100; _gat=1' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' --data "email=${name_email}%40${company_email}.${suffix}&reason=${reason}&message=Yo+waddup"
+ 	curl 'http://patrickoboyle.com/send_contact_form_email.php' -H 'Host: patrickoboyle.com' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' --compressed -H 'Referer: http://patrickoboyle.com/' -H 'Cookie: _ga=GA1.2.1618244444.1475148100; _gat=1' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' --data "email=${name_email}%40${company_email}.${suffix}&reason=${reason}&message=${message}"
 
 	printf "\n\nEmail sent from ${name_email}@${company_email}.${suffix}\n\n"
 	for i in `seq 1 50`; do
